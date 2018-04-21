c75dc5be3e33d1fd1d1beb189e63373f49e3a882
Aaron <ledzeppelindrummer@gmail.com>
2018-04-21 11:57:29 -0400

    Did task 3, outputting to a csv file

--- main.py
+++ main.py
@@ -20,11 +20,16 @@ def shift_by(shift, inputStr):
 if __name__ == '__main__':
     random.seed()
     plaintext_file = open("plaintext.txt", "r")
-    file_out = open("output.txt", "w")
+    file_out = open("output.csv", "w")
 
     plaintext = plaintext_file.readline()
+    secret = shift_by(random.randint(0,26), plaintext)
 
-    file_out.write(shift_by(random.randint(0,26), plaintext))
+    file_out.write( "?,"+ secret + "\n")
+
+    for x in range(1,26):
+        file_out.write( str(x) + "," + shift_by(x, secret) + "\n")
+
 
     plaintext_file.close()
     file_out.close()
